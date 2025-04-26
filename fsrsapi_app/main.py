45586a2bea94f89f81d5e10eac0a7565e3644b0c
Lisseth Martinez <lis.seth2k16@gmail.com>
2025-04-26 00:48:18 -0600

    Endpoint for screen recomendations

--- fsrsapi_app/main.py
+++ fsrsapi_app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
 from .endpoints import review_card
-
+from .endpoints import screen_recomendations
 app = FastAPI()
 
 app.include_router(review_card.router, prefix="/fsrs")
+app.include_router(screen_recomendations.router, prefix="/fsrs")
