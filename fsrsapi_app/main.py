4b0857031b211a9303e9c63b7e689406fc1886b0
Edgar Gonzalez <alejgc99@gmail.com>
2025-05-06 18:56:49 -0600

    fix

--- fsrsapi_app/main.py
+++ fsrsapi_app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 
 from .endpoints import review_card
-from .endpoints import screen_recomendations
 from .endpoints import init_card
 app = FastAPI()
 
 app.include_router(review_card.router, prefix="/fsrs")
-app.include_router(screen_recomendations.router, prefix="/fsrs")
 app.include_router(init_card.router, prefix="/fsrs")
